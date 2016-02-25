be5f8e3e70c1e46384adc48de2d7f8641994c159
jacqlesage <jacqlesages@gmail.com>
2016-02-25 22:41:46 +1300

    added some general exception to UserSignUp servlet. Next steps to collect all of the user input and add it to two tables. Check P.K and S.K (email)

---
+++ src/java/viewWeb/UserSignUp.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viewWeb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author James
+ */
+@WebServlet(name = "UserSignUp", urlPatterns = {"/UserSignUp"})
+public class UserSignUp extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+              /* TODO output your page here. You may use following sample code. */
+        System.out.println("User signup servlet");
+        String emailAddress = request.getParameter("Email");
+        String password = request.getParameter("Password");;
+        
+        try {
+            //cda.findCustomer(emailAddress, password);
+            
+            //if the
+            //if(CustomerDAO.customerFound == true){
+                
+                response.sendRedirect("http://localhost:9999/index.jsp");
+            
+          //read up on what errors I need and which exceptions I should be using here  
+        } catch (Exception ex) {
+            Logger.getLogger(LogInServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
