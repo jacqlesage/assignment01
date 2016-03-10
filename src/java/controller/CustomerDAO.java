ba23dd2ba91ce5605290ac9c5f7989b386e03863
jacqlesage <jacqlesages@gmail.com>
2016-03-10 22:46:26 +1300

    setting up of new customer DAO method.

--- src/java/controller/CustomerDAO.java
+++ src/java/controller/CustomerDAO.java
@@ -87,6 +87,44 @@ public class CustomerDAO {
     String user_phone, String user_address_1, String user_address_2, String user_region, String user_post_code,
     String user_password, String user_confirm_password, String user_age)
     {
+        //I need to find out how to add a customer number automatically with every sign up and deal with pass hash#
+        //also need to deal with putting the password into another table //logintable
+        String sql = "INSERT INTO customertable user_first_name = ?, user_last_name = ?, user_email =?, "
+                + "user_phone = ?,user_address_1 = ?, user_address_2 = ?, user_region = ?, user_post_code =?,"
+                + " user_password = ?, user_confirm_password= ?,user_age = ?";
+        
+          
+             try {
+                 //change this code to meet new signup code. <- copied from above
+                 //had to add this to register driver for some reason. 
+                 Class.forName("com.mysql.jdbc.Driver");
+       
+                 //connect to DB
+                 Connection con = DriverManager.getConnection(url, "root", "");
+              
+                 //create the statement that you want to find from the string
+                 PreparedStatement stmt = con.prepareStatement(sql);
+                 
+                 stmt.setString(1, email);
+                 stmt.setString(2, password);
+                 ResultSet rs =stmt.executeQuery();
+                
+                 while(rs.next()){
+                 customerFound = true;
+                 //print out to test if somthing is found
+                 System.out.println("found customer");
+                 //customer has been found so we need to now pull out the customer info
+                 //and change the main page to say hello customer name
+                 //also need to make the dash board information available
+                 //else direct the customer back to either a / signup ir b/back to login option
+                 //with some information to show that the user name is incorrect.
+                 }
+                
+                 
+             } catch (SQLException ex) {
+                 System.out.println("no customer found");
+                 Logger.getLogger(CustomerDAO.class.getName()).log(Level.SEVERE, null, ex);
+             }
         
     }
         
