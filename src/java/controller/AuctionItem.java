eba801a444177c5fb4abe2979c5bdf98625207f9
jacqlesage <jacqlesages@gmail.com>
2016-07-11 21:40:34 +1200

    creating an auction item. I then need to add the item to something which will handle the bids for this item. Not soo sure what to do yet but so far have handleAuciton class and an auctionItem class, the Auction Item is just the item that is being auctioned object. Need to create an admin page to be able to add an auction then link all this some how.

---
+++ src/java/controller/AuctionItem.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author James
+ */
+public class AuctionItem {
+    private String auctionTitle;
+    private String auctionPicture;
+    private String auctionDescription;
+    //auction ID? connects via an admin HTML page? 
+    
+            
+}
